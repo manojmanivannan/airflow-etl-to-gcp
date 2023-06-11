a36440f5bdd43b727ac937b3aed465dc3724212c
Manoj Manivannan <manojm18@live.in>
2023-06-11 11:00:43 +0100

    add dag

---
+++ yahoo_dag.py
@@ -0,0 +1,31 @@
+from datetime import timedelta, datetime
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from airflow.utils.dates import days_ago
+from yahoo_etl import extract_data_from_yahoo
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 6, 11),
+    'email': ['airflow@example.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=1)
+}
+
+dag = DAG(
+    'yahoo_dag',
+    default_args=default_args,
+    description='Our first DAG with ETL process!',
+    schedule_interval=timedelta(days=1),
+)
+
+run_etl = PythonOperator(
+    task_id='complete_yahoo_etl',
+    python_callable=extract_data_from_yahoo,
+    dag=dag, 
+)
+
+run_etl
--- yahoo_etl.py
+++ yahoo_etl.py
@@ -4,11 +4,38 @@ from google.cloud import storage
 import yfinance as yf
 from datetime import datetime
 
-msft = yf.Ticker('MSFT')
+bucket_name='yahoo_stock_airflow_bucket'
 
-today = datetime.today().strftime('%d_%m_%Y_%H_%M')
+def extract_data_from_yahoo(stock_name: str='MSFT'):
+    msft = yf.Ticker(stock_name)
 
-# get historical market data
-hist = msft.history(period="1d", interval="1h")
+    today = datetime.today().strftime('%d_%m_%Y_%H_%M')
 
-hist.to_csv(f'yahoo_{today}.csv')
+    # get historical market data
+    hist = msft.history(period="1d", interval="1h")
+
+    hist.to_csv(f'gs://yahoo_stock_airflow_bucket/yahoo_{stock_name}_{today}.csv')
+
+# QDrbywkxdhhZmeHa
+
+
+
+
+    # """Write and read a blob from GCS using file-like IO"""
+    # # The ID of your GCS bucket
+    # # bucket_name = "your-bucket-name"
+
+    # # The ID of your new GCS object
+    # # blob_name = "storage-object-name"
+
+    # storage_client = storage.Client()
+    # bucket = storage_client.bucket(bucket_name)
+    # blob = bucket.blob(blob_name)
+
+    # # Mode can be specified as wb/rb for bytes mode.
+    # # See: https://docs.python.org/3/library/io.html
+    # with blob.open("w") as f:
+    #     f.write("Hello world")
+
+    # with blob.open("r") as f:
+    #     print(f.read())
